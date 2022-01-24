ee8a417ac4a94c4a1f00a1d7f081d3304a58920b
Cifer <abdybaev.azamat@gmail.com>
2022-01-24 12:38:35 +0600

    rename modules

---
+++ app-accounts/src/main/java/kz/abdybaev/banking/app/accountssystem/AccountsSystemApplication.java
@@ -0,0 +1,15 @@
+package kz.abdybaev.banking.app.accountssystem;
+
+import kz.abdybaev.banking.app.accountssystem.config.AppConfig;
+import kz.abdybaev.banking.lib.common.exceptions.BaseControllerAdvice;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Import({AppConfig.class, BaseControllerAdvice.class})
+public class AccountsSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsSystemApplication.class, args);
+    }
+}
